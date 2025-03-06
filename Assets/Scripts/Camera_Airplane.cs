a7458ea3252e03c5349c65fcd2e6f8bc86988201
PeerG7 <peerapon.lukc@bumail.net>
2025-03-06 14:11:53 +0700

    Labs7

---
+++ Assets/Scripts/Camera_Airplane.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float enginePower = 20f;
+    [SerializeField] float liftBooster = 0.5f;
+    [SerializeField] float drag = 0.001f;
+    [SerializeField] float angularDrag = 0.0001f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
